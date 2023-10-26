1d5a3237efb5afce4b2e1f3c38a14573ea76f357
nnh53 <nnh53.work@gmail.com>
2023-10-26 17:46:54 +0700

    form lỏ

--- src/layout/ScreenHome/ScreenHome.js
+++ src/layout/ScreenHome/ScreenHome.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import './ScreenHome.scss'
-import ScreenHomeTitle from './ScreenHomeTitle/ScreenHomeTitle'
-import ScreenHomeRegister from './ScreenHomeRegister/ScreenHomeRegister'
 import ScreenHomeBigForm from './ScreenHomeBigForm/ScreenHomeBigForm'
+import ScreenHomeTitle from './ScreenHomeTitle/ScreenHomeTitle'
 
 export default function ScreenHome() {
   return (
     <div className='container'>
       <ScreenHomeTitle />
-      <ScreenHomeRegister />
       <ScreenHomeBigForm />
     </div>
   )
