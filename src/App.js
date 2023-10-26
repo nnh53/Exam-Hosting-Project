66780754e2dab4fe9cdb1ebc8bb6afe7dbef3678
quyenpq.work@gmail.com <quyenpq.work@gmail.com>
2023-10-26 20:15:06 +0700

    bug fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css'
 import ScreenHome from './layout/ScreenHome/ScreenHome'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ScreenQuiz from './layout/ScreenQuiz/ScreenQuiz'
 
 function App() {
   return (
     <div>
-      <ScreenHome />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ScreenHome />} />
+          <Route path='/quiz' element={<ScreenQuiz />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
