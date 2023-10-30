719b63cc073b3188ec8eba1ecb570b928b7a6580
nnh53 <nnh53.work@gmail.com>
2023-10-30 16:58:10 +0700

    noti fixed

--- src/components/TestAlreadyStartNotification/TestAlreadyStartNotification.js
+++ src/components/TestAlreadyStartNotification/TestAlreadyStartNotification.js
@@ -1,7 +1,7 @@
 import React from "react";
 import SmallForm from "../SmallForm/SmallForm";
 import "./TestAlreadyStartNotification.scss";
-import { Button } from "antd";
+import { Button, ConfigProvider } from "antd";
 import { useNavigate } from "react-router-dom";
 import { getUserInfor } from "../../utils/LocalStorageManagement";
 import { useTimerContext } from "../TimerContext";
@@ -20,7 +20,22 @@ export default function TestAlreadyStartNotification() {
     <div className="TestAlreadyStartNotification">
       <h1>There is a test already started</h1>
       <p>If you submit another account, the old test will ended</p>
-      <Button onClick={movingHandle}> Move back to Test</Button>
+      <ConfigProvider
+        theme={{
+          components: {
+            Button: {
+              contentFontSize: 30,
+              controlHeight: 50,
+            },
+          },
+        }}
+      >
+        <div className="centerButton">
+          <Button type="primary" onClick={movingHandle}>
+            Move back to Test
+          </Button>
+        </div>
+      </ConfigProvider>
     </div>
   );
 }
