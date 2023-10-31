d68b94ca28a4611f22526f4f6fb446f494cf2081
quyenpq.work@gmail.com <quyenpq.work@gmail.com>
2023-10-31 17:37:32 +0700

    error page 50%

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,5 +1,13 @@
 import React from "react";
+import img404 from "../../assets/404 not found.png";
+import "./ErrorPage.scss";
 
 export default function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="ErrorPage">
+      <div className="image-container">
+        <img src={img404} alt="error" />
+      </div>
+    </div>
+  );
 }
